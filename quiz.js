2781bed9c4222f30d63788759e04eacc2d0bbab2
Chris DeLauder & Chris Cooper <pair+cdelauder.coopers@devbootcamp.com>
2014-06-13 13:34:54 -0700

    unsuccessfully used firebase

--- quiz.js
+++ quiz.js
@@ -1,3 +1,6 @@
-var fburl = new Firebase("https://flickering-fire-8809.firebaseIO.com")
-fburl.set({ question: "Who is playing in the Stanley Cup Finals?", answer1: "Kings", answer2: "Blackhawks" })
-document.append(fburl)
+var fb = new Firebase("https://flickering-fire-8809.firebaseIO.com")
+fb.set({ question: "Who is playing in the Stanley Cup Finals?" })
+question1.on('value', function(q) {
+  var q = question.val() ?  q.val().question : "";
+  alert(q))
+})
