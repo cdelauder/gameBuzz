dcda2a14d11ba8d61fc2426f601c14a9e010fb15
Chris DeLauder & Chris Cooper <pair+cdelauder.coopers@devbootcamp.com>
2014-06-13 18:03:49 -0700

    we queried the firebase database

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 var fb = new Firebase("https://flickering-fire-8809.firebaseIO.com")
-fb.set({ question: "Who is playing in the Stanley Cup Finals?" })
-question1.on('value', function(q) {
+$(document).onload( fb.set({ question: "Who is playing in the Stanley Cup Finals?" }))
+fb.on('value', function(q) {
   var q = question.val() ?  q.val().question : "";
   alert(q))
 })
