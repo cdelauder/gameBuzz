d7ab18c53267b7a4cdacbe5ff257c390b9506140
betsyrhodes <betzrhodes@gmail.com>
2014-06-13 22:01:24 -0700

    log out buggy facebook code included but not working and commented out

--- public/js/application.js
+++ public/js/application.js
@@ -1,30 +1,34 @@
-// $(document).ready(function() {
-//   // This is called after the document has loaded in its entirety
-//   // This guarantees that any elements we bind to will exist on the page
-//   // when we try to bind to them
+$(document).ready(function() {
+  var gameBuzz = new Firebase('https://gamebuzz.firebaseio.com');
 
-//   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-// });
-
-var gameBuzz = new Firebase('https://gamebuzz.firebaseio.com');
-
-var auth = new FirebaseSimpleLogin(gameBuzz, function(error, user) {
+  auth = new FirebaseSimpleLogin(gameBuzz, function(error, user) {
     if (error) {
       // an error occurred while attempting login
       // code: 'INVALID_PASSWORD',
       // message: 'The specified password is incorrect.'
       alert(error);
     } else if (user) {
-      // user authenticated with Firebase
-      alert('User ID: ' + user.uid + ', Provider: ' + user.provider);
+      console.log('User ID: ' + user.uid + ', Provider: ' + user.provider);
     } else {
-      // user is logged out
+      auth.login('facebook')
     }
   });
+    bindEventListeners()
 
-  auth.login('facebook', {
-    rememberMe: true,
-    scope: 'email,user_likes'
-  });
+});
 
-  auth.logout();
+function bindEventListeners() {
+  $('button').on('click', facebookAdios)
+}
+
+function facebookAdios() {
+  console.log('fuck!!!!')
+  // FB.logout(function(response) {
+  //     // Person is now logged out
+  // });
+  auth.logout()
+}
+
+
+
+
