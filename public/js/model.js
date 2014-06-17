437587d1d143080e6c96b8efdf78ad0b92c5d2f2
Chris DeLauder <cdelauder@gmail.com>
2014-06-17 07:50:09 -0700

    created a firebase module

--- public/js/model.js
+++ public/js/model.js
@@ -123,3 +123,61 @@ var User = {
     return this.authToken;
   }
 };
+
+//////////////firebase module
+
+var firebase = {
+  makeLink: function() {
+    return this.dbLink = new Firebase('https://gamebuzz.firebaseio.com')    
+  },
+
+  GetDbLink: function() {
+    return this.dbLink
+  },
+
+  makeUserLink: function() {
+    return this.userDbLink = new Firebase('https://gamebuzz.firebaseio.com/-JPbgRPRsqDJNz37rMVs/users')
+  },
+
+  getUserDbLink: function() {
+    return this.getUserDbLink
+  },
+
+  makeQuestionDbLink: function() {
+    return this.questionDbLink = new Firebase('https://gamebuzz.firebaseio.com/-JPbgRPRsqDJNz37rMVs/questions')
+  },
+
+  getQuestionDbLink: function() {
+    return this.questionDbLink
+  },
+
+  makeGameDbLink: function() {
+    return this.gameDbLink = new Firebase('https://gamebuzz.firebaseio.com/-JPbgRPRsqDJNz37rMVs/games')
+  },
+
+  getGameDbLink: function() {
+    return this.gameDbLink
+  },
+
+  authenticate: function() {
+    var that = this;
+    this.auth = new FirebaseSimpleLogin(that.dbLink, function(error, user) {
+      if (error) {
+        alert(error);
+      } else if (user) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  },
+
+  login: function() {
+    this.auth.login('facebook')
+  },
+
+  logout: function() {
+    this.auth.logout()
+  },
+
+};
